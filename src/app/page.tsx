5b9ab9ff0c5fe06e72b0ed1e08311640f9d45ba9
Abhishek Yadav <148934908+Abhidev-aiml@users.noreply.github.com>
2025-02-19 00:45:43 +0530

    added base app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import { BackgroundLines } from "@/components/ui/background-lines";
 
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen justify-center items-center bg-black text-white">
+    <div className="flex min-h-screen justify-center items-center ">
+      <BackgroundLines className="bg-black flex justify-center items-center min-h-screen text-white text-2xl font-semibold shadow-lg ">
+
        MindGuard – AI-Driven Mental Health Companion for University Students
+      </BackgroundLines>
     </div>
   );
 }
